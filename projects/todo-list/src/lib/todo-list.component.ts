bf47f1e059eca1c52b3fdb771ffc487219db0ce7
Darchevil <yderneville@gmail.com>
2024-01-05 00:25:10 +0100

    creating application todo list

---
+++ projects/todo-list/src/lib/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-todo-list',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      todo-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class TodoListComponent {
+
+}
---
+++ projects/todo-list/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of todo-list
+ */
+
+export * from './lib/todo-list.service';
+export * from './lib/todo-list.component';
