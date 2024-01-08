7188f69a8c05460264780cd6bbc46566d14ff359
Darchevil <yderneville@gmail.com>
2024-01-08 08:28:21 +0100

    adding first task into list

--- projects/todo-src/src/app/components/todo-list/todo-list-element/todo-list-element.component.ts
+++ projects/todo-src/src/app/components/todo-list/todo-list-element/todo-list-element.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../../types/task';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'todo-list-element',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './todo-list-element.component.html',
   styleUrl: './todo-list-element.component.css',
 })
-export class TodoListElementComponent {
+export class TodoListElementComponent implements OnInit {
   //child
-  //Créer une task et l'envoie au parent todoList
+  listTask: Array<Task> = [];
+  task: Task = { title: 'test', done: false };
+
+  ngOnInit(): void {
+    this.listTask.push(this.task);
+  }
 }
--- projects/todo-src/src/app/components/todo-list/todo-list.component.ts
+++ projects/todo-src/src/app/components/todo-list/todo-list.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoListElementComponent } from './todo-list-element/todo-list-element.component';
+import { Task } from '../../types/task';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [TodoListElementComponent],
+  imports: [TodoListElementComponent, NgFor],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css',
 })
 export class TodoListComponent {
   //Parent component
   //Possède une liste de task et c'est lui qui update les task et qui peut agir sur les tasks
+  // updateListTask(taskToPush: Task) {
+  //   this.listTask.push(taskToPush);
+  // }
 }
